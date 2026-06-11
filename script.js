5104853d2152f203c24abfc8783e4204488a4bf9
reatiguiadriana901 <reatiguiadriana901@gmail.com>
2026-06-11 06:01:20 -0500

    feat: :sparkles: agregar javascript con funciones

--- script.js
+++ script.js
@@ -11,12 +11,54 @@ async function consumirAPI(endpoint) {
 }
 
 
-async function ActividadReciente() {
+async function actividadReciente() {
     const ahora = new Date();
     const hace24h = new Date(ahora.getTime() - 24 * 60 * 60 * 1000);
 
-    let endpoint = `format=geojson&starttime=${hace24h.toISOString().split("T")[0]}&endtime=${ahora.toISOString().split("T")[0]}`;
+    let endpoint = `?format=geojson&starttime=${hace24h.toISOString().split("T")[0]}&endtime=${ahora.toISOString().split("T")[0]}`;
 
     const datos = await consumirAPI(endpoint);
+
+    
+    mostrarResultados(datos)
+
+}
+
+function mostrarResultados(datos){
+    
+    console.log(datos.features)
+    const resultado = document.querySelector(".resultados");
+    resultado.innerHTML = datos.features.map(feature => `
+    <div class="sismos">
+        <h3>${feature.properties.mag}</h3>
+        <p>${feature.properties.place}</p>
+        <p>${new Date(feature.properties.time).toLocaleDateString()}</p>
+
+    </div>
+`).join("");
+    
+}
+
+async function buscarPorMagnitud(param) {
+
+    const magnitud = document.getElementById("input_magnitud").value
+
+    let endpoint = `?format=geojson&minmagnitude=${magnitud}`
+
+    const datos = await consumirAPI(endpoint);
+
+    mostrarResultados(datos)
+}
+
+async function buscarPorRango(rango){
+
+    const inicio = document.getElementById("input_inicio").value;
+    const final = document.getElementById("input_final").value;
+
+    let endpoint = `?format=geojson&starttime=${inicio}&endtime=${final}`;
+
+    const datos = await consumirAPI(endpoint);
+
+    mostrarResultados(datos)
 
 }
